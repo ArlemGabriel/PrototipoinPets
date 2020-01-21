aac86263a895b9d31999bc32b89f87575dc98f74
JeanVegaD <jean0798@gmail.com>
2020-01-21 13:42:13 -0600

    cambios

--- scripts/owner_dasboard.js
+++ scripts/owner_dasboard.js
@@ -21,7 +21,6 @@ function cargarMascotas(){
     var html=[]
 
     for(var i=0;i<5;i++){
-        console.log("entre");
         //se obtienen las variables necesarias 
         var nombreMascota="Mascota 1";
         var img="https://arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/CD5DKS2JJVHFFMGMNOXOGDNWNY.jpg";
@@ -69,5 +68,142 @@ function cargarMascotas(){
 }
 
 
+
+
+/*
+   abre los permisos acutales del usuarios
+*/
+function abrirPermisos(){
+
+    var html=[]
+
+    for(var i=0;i<3;i++){
+        //se obtienen las variables necesarias 
+        var nombreMascota="Puppy";
+        var idMascota="5485";
+        var veterinario="Elmer Figueroa";
+        var myvar = '<div class="contenedorPermisos">'+
+'                        <div class="row">'+
+'                            <div class="col-md-8">'+
+'                                <h4><i class=\'fas fa-user-md\'></i>'+veterinario+'</h4>'+
+'                                <h4><i class=\'fas fa-paw\'></i>'+nombreMascota+' # '+idMascota+'</h4>'+
+'                            </div>'+
+'                            <div class="col-md-4 align-self-center"><button class="buttonRed" onclick="revocarAcceso()">Revocar acceso</button></div>'+
+'                        </div>'+
+'                    </div>';
+        html.push(myvar)
+    }
+
+    $('#contenedormodaPermisos').append(html)
+
+}
+
+
+/*
+    Revoca los permisos de un veterinario a una mascotas
+*/
+function revocarAcceso(){
+
+}
+
+
+/*
+    Abre el editar pefil y carga los datos actuales
+*/
+function abrirEditarPerfil(){
+    //carga las variables para mostrarlas
+    var nombre="Jean Anthony";
+    var apellidos="Vega Diaz";
+    var correo="Jean0798@gmail.com";
+    //muestra las variables en los campos
+    $("#modalNombre").val(nombre);
+    $("#modalApellidos").val(apellidos);
+    $("#modalCorreo").val(correo);
+}
+
+/*
+    guarda los nuevos datos del perfil
+*/
+function modificarPerfil(){
+
+}
+
+
+
+
+/*
+    Abre y carga las notificaciones en el modal
+*/
+function abrirNotificaciones(){
+
+    var html=[]
+
+    for(var i=0;i<3;i++){
+        //se obtienen las variables necesarias 
+        var nombreMascota="Puppy";
+        var idMascota="5485";
+        var veterinario="Elmer Figueroa";
+
+        var myvar = '<div class="contenedorPermisos">'+
+        '                        <div class="row">'+
+        '                            <div class="col-md-8">'+
+        '                                <h4><i class=\'fas fa-user-md\'></i>'+veterinario+'</h4>'+
+        '                                <h4><i class=\'fas fa-paw\'></i>'+nombreMascota+' #'+idMascota+'</h4>'+
+        '                            </div>'+
+        '                            <div class="col-md-4">'+
+        '                                <div>'+
+        '                                    <button class="buttonGreen" onclick="aceptarNotificacion()">Aceptar</button>'+
+        '                                    <button class="buttonRed" onclick="descartarNotificacion()">Eliminar</button>'+
+        '                                </div>'+
+        '                                '+
+        '                            </div>'+
+        '                        </div>'+
+        '                    </div>';
+	
+
+
+        html.push(myvar)
+    }
+    $('#contenedormodalNotificaciones').append(html)
+}
+
+
+/*
+    Acepta o descarta la notificacion
+*/
+
+function aceptarNotificacion(){
+
+}
+
+function descartarNotificacion(){
+
+}
+
+
+
+/*
+    Agrega una nueva mascota
+*/
+function agregarMascota(){
+
+}
+
+
+/*
+    Realiza la funcion de buscar mascota
+*/
+function buscarMascota(){
+    var busqueda = $("#input_search").val();
+
+    //se realiza la busqueda
+}
+
+
+
+
+
+
 cargarPerfil();
 cargarMascotas();
+abrirPermisos();
